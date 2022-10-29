92a22c477b54f5c9e96631fa6e83f34b154dd458
GYH1208 <geyuheng1208@gmail.com>
2022-10-29 02:47:23 -0700

    explore updated

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,8 +1,60 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+const synth = window.speechSynthesis;
+const inputTxt = document.querySelector("#text-to-speak");
+const voiceSelect = document.querySelector('select');
+const pitch = document.querySelector('#pitch');
+const rate = document.querySelector('#rate');
+var voices = [];
 
 function init() {
   // TODO
-  document.addEventListener()
+  
+  populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+inputForm.onsubmit = (event) => {
+  event.preventDefault();
+
+  const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+  const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+  for (let i = 0; i < voices.length ; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+  utterThis.pitch = pitch.value;
+  utterThis.rate = rate.value;
+  synth.speak(utterThis);
+
+}
+
+   document.querySelector('button').addEventListener('click',readout);
+
+}//end of init()
+
+function readout(){
+  
+
+}
+
+
+function populateVoiceList(){
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length ; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += ' — DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
 }
